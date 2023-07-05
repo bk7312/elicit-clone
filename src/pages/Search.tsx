159af86f183593fd399fd350409b09b388e7b089
bk7312 <bk7312@users.noreply.github.com>
2023-07-05 14:21:06 +0800

    created sidebar and results layout
    - added layout and styling to Sidebar
    - added layout and styling to Results
    - created ResultCard

--- src/pages/Search.tsx
+++ src/pages/Search.tsx
@@ -1,16 +1,35 @@
 
 import Sidebar from '../components/Sidebar'
 import Results from '../components/Results'
-// import { Heading, Text, Button } from '@chakra-ui/react'
+import { 
+  Flex,
+  // Heading, 
+  // Text, 
+  // Button,
+} from '@chakra-ui/react'
 
 export default function Search() {
 
-  // sidebar, results
+  // searchbar in header (!!)
+
+  // sidebar and results in flex container, toggle show/hide sidebar
+  // pass search params to sidebar as searchInput
+  // add dummy data
+  // add logic to handle results filter
+  // implement result modal
+  // full page height background
+
+  // sidebar should be fixed in position and take up at most the whole screen height
+  // shrinking down the summary and infoButton section as required, up to a minimum height
+
+  // results should scroll with the page
+
+  // further improvement on result, sticky the filter/sortby header?
 
   return (
-    <>
-      <Sidebar/>
+    <Flex backgroundColor='gray.50'>
+      <Sidebar searchInput='Placeholder search term'/>
       <Results/>
-    </>
+    </Flex>
   )
 }
